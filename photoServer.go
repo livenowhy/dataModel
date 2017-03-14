0ff4d48e90c6577f92fb6ab22a193458736f4f64
zhangpei.liu <liuzhangpei@126.com>
2017-03-14 14:06:49 +0800

    ....

--- photoServer.go
+++ photoServer.go
@@ -1,9 +1,13 @@
 package dataModel
 
+type ResourceQuery struct {
+	QueryParameter []ResourceLocation `json:"queryparameter"`
+}
+
+
 type ResourceLocation struct {
 	// 资源位置
-
-	Team_uuid string `json:"-" db:"team_uuid"`  // 组织 uuid 区分不同用户
+	Team_uuid string `json:"team_uuid" db:"team_uuid"`  // 组织 uuid 区分不同用户
 	Resource_type   string `json:"resource_type" db:"resource_type"`     // 资源类型, (用户头像,镜像头像)
 	Resource_uuid   string `json:"resource_uuid" db:"resource_uuid"`     // 资源的 uuid (用户头像采用用户组织的UUID, 镜像图像采用镜像的uuid)
 	Resource_domain string `json:"resource_domain" db:"resource_domain"` // 资料所属的域(前端预定即可)
@@ -18,3 +22,6 @@ type CallbackActionType struct {
 	Width             string `json:"width"`
 	ImageUrl          string `json:"imageurl"`
 }
+
+
+// storage_uuid  team_uuid resource_type resource_uuid resource_domain storage_url   create_time  update_time
